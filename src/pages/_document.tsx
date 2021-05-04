9e008bce956896c5808ec00d099a7b154c2219b3
Bruno Futema <bruno.futema@outlook.com>
2021-05-03 21:04:45 -0300

    feat: :sparkles: Add chakra ui libs

---
+++ src/pages/_document.tsx
@@ -0,0 +1,26 @@
+import Document, { Head, Html, Main, NextScript } from 'next/document';
+
+class MyDocument extends Document {
+  render(): JSX.Element {
+    return (
+      <Html lang="pt-br">
+        <Head>
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
+
+          <link
+            rel="stylesheet"
+            href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600;700&display=swap"
+          />
+
+          <link rel="shortcut icon" href="/favicon.png" type="image/png" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
